389da7a3e43672141ae376bf2c39598a31bf6c8b
Tom-Inge Larsen <tom@codesalot.com>
2016-11-05 00:30:39 +0100

    Cleaned up

--- Test.ps1
+++ Test.ps1
@@ -2,9 +2,7 @@ param (
     [object]$WebhookData
 )
 
-Write-Output "Run 1"
-$uri = "https://outlook.office365.com/webhook/8a6d5da3-6c84-4ba4-8e0d-b0438762d841@579d2ccd-5d17-45ad-8ea6-96b760e70ad3/IncomingWebhook/59c4c8dbc9364e79a77017c71e7dcbed/1c84c841-d2d6-4cd6-955b-cd152cd554a8"
-
+$uri = Get-AutomationVariable -Name 'teamsuri'
 
 # If runbook was called from Webhook, WebhookData will not be null.
 if ($WebhookData -ne $null) {
@@ -13,13 +11,10 @@ if ($WebhookData -ne $null) {
 
     $params = ConvertFrom-Json -InputObject $WebhookBody
 
-    $txt = $params.txt + " - Postet via Azure automation"
-
-    Write-Output "Run 2"
+    $txt = $params.txt + " - Posted via Azure automation"
 }
 else {
-    $txt =  "Postet via Azure automation, men params virket ikke..."
-    Write-Output "Run 3"
+    $txt =  "Posted via Azure automation, webhookdata didn't work tho..."
 }
 
 $body = ConvertTo-JSON @{
@@ -27,4 +22,3 @@ $body = ConvertTo-JSON @{
 }
 
 irm -uri $uri -Method Post -body $body -ContentType 'application/json'
-Write-Output "Run 4"
